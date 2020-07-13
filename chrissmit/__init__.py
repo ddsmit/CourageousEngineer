e10f07e13f28954efbcfe8696f34af0d505aa064
ddsmit <david.d.smit@gmail.com>
2020-07-12 23:03:26 -0400

    Added secure headers

--- chrissmit/__init__.py
+++ chrissmit/__init__.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from chrissmit.views import blueprints
+from secure import SecureHeaders
 
+secure_headers = SecureHeaders()
 
 app = flask.Flask(__name__)
 db = SQLAlchemy(app)
@@ -18,3 +20,8 @@ app.config['SECRET_KEY'] = Config.SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 app.config['RECAPTCHA_PUBLIC_KEY'] = Config.RECAPTCHA_PUBLIC_KEY
 app.config['RECAPTCHA_PRIVATE_KEY'] = Config.RECAPTCHA_PRIVATE_KEY
+
+@app.after_request
+def set_secure_headers(response):
+    secure_headers.flask(response)
+    return response
